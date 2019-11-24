8272ca24c051ef41fc7ddd152c6041e53ba1d60c
el-beth <enduad@gmail.com>
2019-11-24 13:57:45 +0300

    second commit

---
+++ today.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+todayDate=$(date | grep -oe "[A-Za-z]\{3\}\ [0-9]\{1,2\}\ [A-Za-z]\{3\}");
+userName=$(whoami)
+
+if [ "$userName" == "root" ]
+	then echo "run this script only as not-root"
+else if [ ! -d "$todayDate" ]
+	then mkdir "$HOME/Desktop/surfDump/$todayDate";
+	fi;
+fi;
+
+
+
